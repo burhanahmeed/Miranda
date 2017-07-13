c2b96bde15bde0e055657165248eb6dba62c9c57
AlphaMGK <septian.alfarizi.21@gmail.com>
2017-07-13 15:39:17 +0700

    encapsulate UserInfo

--- app/src/main/java/com/ayotong/miranda/model/UserInfo.java
+++ app/src/main/java/com/ayotong/miranda/model/UserInfo.java
@@ -10,10 +10,122 @@ public class UserInfo {
     public static final int DATABASE_VERSION = 1;
     public static final String TABLE_CREATE =
             "create table if not exists UserInfo (id integer primary key, username text, age integer, gender text, weight integer, height integer, is_pregnant integer, is_nap integer,start_nap text, start_sleep text);";
-    public static final String COL_ID = "quest_id";
-    public static final String COL_CATEGORY = "category";
-    public static final String COL_TIME = "time";
-    public static final String COL_EXP = "exp";
-    public static final String COL_QUESTDESC = "quest_desc";
-    public static final int QUEST_ID = -1;
+    public static final String COL_ID = "userinfo_id";
+    public static final String COL_UNAME = "username";
+    public static final String COL_AGE = "age";
+    public static final String COL_GENDER = "gender";
+    public static final String COL_WEIGHT = "weight";
+    public static final String COL_HEIGHT = "height";
+    public static final String COL_ISPREGNANT = "is_pregnant";
+    public static final String COL_ISNAP = "is_nap";
+    public static final String COL_STARTNAP = "start_nap";
+    public static final String COL_STARTSLEEP = "start_sleep";
+    public static final int USER_ID = -1;
+
+    private int id;
+    private String username;
+    private int age;
+    private String gender;
+    private int weight;
+    private int height;
+    private boolean ispregnant;
+    private boolean isnap;
+    private String startnap;
+    private String startsleep;
+
+    public UserInfo(){
+
+    }
+
+    public UserInfo(int id, String username, int age, String gender, int weight, int height, boolean ispregnant, boolean isnap, String startnap, String startsleep){
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.ispregnant = ispregnant;
+        this.isnap = isnap;
+        this.startnap = startnap;
+        this.startsleep = startsleep;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public boolean ispregnant() {
+        return ispregnant;
+    }
+
+    public void setIspregnant(boolean ispregnant) {
+        this.ispregnant = ispregnant;
+    }
+
+    public boolean isnap() {
+        return isnap;
+    }
+
+    public void setIsnap(boolean isnap) {
+        this.isnap = isnap;
+    }
+
+    public String getStartnap() {
+        return startnap;
+    }
+
+    public void setStartnap(String startnap) {
+        this.startnap = startnap;
+    }
+
+    public String getStartsleep() {
+        return startsleep;
+    }
+
+    public void setStartsleep(String startsleep) {
+        this.startsleep = startsleep;
+    }
 }
