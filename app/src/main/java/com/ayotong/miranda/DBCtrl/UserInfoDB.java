1a43d13b7c08135a6a75b747a59792eff8dbb1f5
AlphaMGK <septian.alfarizi.21@gmail.com>
2017-07-14 17:34:53 +0700

    UserInfoDB

--- app/src/main/java/com/ayotong/miranda/DBCtrl/UserInfoDB.java
+++ app/src/main/java/com/ayotong/miranda/DBCtrl/UserInfoDB.java
@@ -1,5 +1,6 @@
 package com.ayotong.miranda.DBCtrl;
 
+import android.content.ContentValues;
 import android.content.Context;
 
 import com.ayotong.miranda.model.UserInfo;
@@ -15,5 +16,22 @@ public class UserInfoDB extends DatabaseDAO implements UserInfoDAO {
     public UserInfoDB(Context context){
         super(context, DatabaseDAO.dName, UserInfo.TABLE_CREATE, UserInfo.DATABASE_TABLE, UserInfo.DATABASE_VERSION);
         this.context = context;
+    }
+
+    public UserInfo insert(int ID, UserInfo userinfo){
+        ContentValues cv = new ContentValues();
+        cv.put(UserInfo.COL_ID, ID);
+        cv.put(UserInfo.COL_UNAME, userinfo.getUsername());
+        cv.put(UserInfo.COL_AGE, userinfo.getAge());
+        cv.put(UserInfo.COL_GENDER, userinfo.getGender());
+        cv.put(UserInfo.COL_WEIGHT, userinfo.getWeight());
+        cv.put(UserInfo.COL_HEIGHT, userinfo.getHeight());
+        cv.put(UserInfo.COL_ISPREGNANT, userinfo.ispregnant());
+        cv.put(UserInfo.COL_ISNAP, userinfo.isnap());
+        cv.put(UserInfo.COL_STARTNAP, userinfo.getStartnap());
+        cv.put(UserInfo.COL_STARTSLEEP, userinfo.getStartsleep());
+
+        return new UserInfo((int)super.insert(UserInfo.DATABASE_TABLE, cv), userinfo.getUsername(), userinfo.getAge(), userinfo.getGender(),
+                userinfo.getWeight(), userinfo.getHeight(), userinfo.ispregnant(), userinfo.isnap(), userinfo.getStartnap(), userinfo.getStartsleep());
     }
 }
