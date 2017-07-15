9da8732020290857555ddc7098f1fc730db525ef
Burhanuddin Ahmad (Burhan Med) <burhanahmeed@gmail.com>
2017-07-15 14:12:33 +0700

    splash while if
    unfixed

---
+++ app/src/main/java/com/ayotong/miranda/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.ayotong.miranda;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by burhan on 15/07/17.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_signup);
+
+    }
+}
