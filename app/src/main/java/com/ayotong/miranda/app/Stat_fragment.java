d71537c26a0cbf2ed12cdfe1828fa4fb9e4798c2
Burhanuddin Ahmad (Burhan Med) <burhanahmeed@gmail.com>
2017-07-15 17:23:07 +0700

    Revert "Revert "chart not fix yet""
    This reverts commit 24ffcd9ca9948e9c0376574bc3cc03ea01f14d36.

--- app/src/main/java/com/ayotong/miranda/app/Stat_fragment.java
+++ app/src/main/java/com/ayotong/miranda/app/Stat_fragment.java
@@ -1,5 +1,7 @@
 package com.ayotong.miranda.app;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,13 +10,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.ayotong.miranda.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.ChartData;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.Chart;
+import lecho.lib.hellocharts.view.LineChartView;
+
+import static android.R.attr.type;
+
 public class Stat_fragment extends Fragment {
+
+    private Context context;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.splash_screen_signup, container, false);
+        View v =inflater.inflate(R.layout.fragment_stat, container, false);
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        LineChartView chart = new LineChartView(context);
+        chart.setLineChartData(data);
+
+        return v;
     }
 }
