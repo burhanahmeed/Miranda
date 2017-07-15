445125a697cb5dcc8be605ecd254a5232c75dd9c
Burhanuddin Ahmad (Burhan Med) <burhanahmeed@gmail.com>
2017-07-15 20:35:00 +0700

    undo chart

--- app/src/main/java/com/ayotong/miranda/app/Stat_fragment.java
+++ app/src/main/java/com/ayotong/miranda/app/Stat_fragment.java
@@ -10,18 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.ayotong.miranda.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import lecho.lib.hellocharts.model.ChartData;
-import lecho.lib.hellocharts.model.ColumnChartData;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.view.Chart;
-import lecho.lib.hellocharts.view.LineChartView;
-
-import static android.R.attr.type;
 
 public class Stat_fragment extends Fragment {
 
@@ -33,23 +21,6 @@ public class Stat_fragment extends Fragment {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
         View v =inflater.inflate(R.layout.fragment_stat, container, false);
-
-        List<PointValue> values = new ArrayList<PointValue>();
-        values.add(new PointValue(0, 2));
-        values.add(new PointValue(1, 4));
-        values.add(new PointValue(2, 3));
-        values.add(new PointValue(3, 4));
-
-        //In most cased you can call data model methods in builder-pattern-like manner.
-        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
-        List<Line> lines = new ArrayList<Line>();
-        lines.add(line);
-
-        LineChartData data = new LineChartData();
-        data.setLines(lines);
-
-        LineChartView chart = new LineChartView(context);
-        chart.setLineChartData(data);
 
         return v;
     }
